bc97004b1212849b3f45d4d61271aaaf5be7b838
Longjie Guo <lguo76@wisc.edu>
2022-12-18 22:21:20 -0600

    may have fixed it

--- client.c
+++ client.c
@@ -21,7 +21,8 @@ int main(int argc, char *argv[]) {
     char message[BUFFER_SIZE];
 
     char* request = argv[1];
-    MFS_Init("localhost", 2133);
+    MFS_Init("localhost", 1000);
+    int rc = UDP_FillSockAddr(&addrSnd, "localhost", 2133);
 
     if(strcmp(request, "1") == 0){
             MFS_Init("localhost", 2133);
